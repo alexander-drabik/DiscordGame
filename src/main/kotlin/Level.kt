7bcf6b28978a71c5e9de2953943cc0283b96f31e
alexander-drabik <xelada91@o2.pl>
2022-10-29 16:24:46 +0200

    Add movement

--- src/main/kotlin/Level.kt
+++ src/main/kotlin/Level.kt
@@ -1,2 +1,27 @@
 object Level {
+    const val size = 5
+
+    var layout = mutableListOf(
+        0, 0, 0, 0, 0,
+        0, 1, 0, 0, 0,
+        0, 0, 0, 0, 0,
+        0, 0, 2, 0, 0,
+        0, 0, 0, 0, 0
+    )
+
+    fun generateMessage(): String {
+        var level = ""
+        for ((index, pole) in Level.layout.withIndex()) {
+            if (index%5 == 0) {
+                level += '\n'
+            }
+            when (pole) {
+                0 -> level += ":black_large_square:"
+                1 -> level += ":grinning:"
+                2 -> level += ":white_square_button:"
+            }
+        }
+
+        return level
+    }
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,26 +1,92 @@
+import Level.layout
+import Level.size
+import dev.kord.common.entity.ButtonStyle
 import dev.kord.core.Kord
+import dev.kord.core.behavior.channel.createMessage
+import dev.kord.core.behavior.edit
+import dev.kord.core.behavior.interaction.respondPublic
+import dev.kord.core.event.interaction.ButtonInteractionCreateEvent
 import dev.kord.core.event.message.MessageCreateEvent
 import dev.kord.core.on
 import dev.kord.gateway.Intent
 import dev.kord.gateway.PrivilegedIntent
+import dev.kord.rest.builder.component.ActionRowBuilder
+import dev.kord.rest.builder.message.create.actionRow
+import dev.kord.rest.builder.message.modify.actionRow
+import io.ktor.client.utils.*
 
 suspend fun main() {
     val kord = Kord({}.javaClass.getResource("token")?.readText()!!)
 
-    kord.on<MessageCreateEvent> { // runs every time a message is created that our bot can read
-
-        // ignore other bots, even ourselves. We only serve humans here!
+    kord.on<MessageCreateEvent> {
         if (message.author?.isBot != false) return@on
 
-        // check if our command is being invoked
-        if (message.content != "!ping") return@on
-        //
-        // all clear, give them the pong!
-        message.channel.createMessage("pong!")
+        when (message.content) {
+            "!play" -> {
+                message.channel.createMessage {
+                    content = Level.generateMessage()
+                    actionRow {
+                        interactionButton(ButtonStyle.Primary, "left") {
+                            label = "Lewo"
+                        }
+                        interactionButton(ButtonStyle.Primary, "up") {
+                            label = "Góra"
+                        }
+                        interactionButton(ButtonStyle.Primary, "down") {
+                            label = "Dół"
+                        }
+                        interactionButton(ButtonStyle.Primary, "right") {
+                            label = "Prawo"
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    kord.on<ButtonInteractionCreateEvent> {
+        for ((index, pole) in Level.layout.withIndex()) {
+            if (pole == 1) {
+                Level.layout[index] = 0
+                when (interaction.componentId) {
+                    "up" -> {
+                        Level.layout[if (index - 5 >= 0) index - 5 else index] = 1
+                    }
+                    "down" -> {
+                        Level.layout[if (index + 5 < Level.size*Level.size) index + 5 else index] = 1
+                    }
+                    "left" -> {
+                        Level.layout[if ((index - 1)%5 != 4) index - 1 else index] = 1
+                    }
+                    "right" -> {
+                        Level.layout[if ((index + 1)%5 != 0) index + 1 else index] = 1
+                    }
+                }
+                break
+            }
+        }
+        interaction.message.edit {
+            content = Level.generateMessage()
+            actionRow {
+                interactionButton(ButtonStyle.Primary, "left") {
+                    label = "Lewo"
+                }
+                interactionButton(ButtonStyle.Primary, "up") {
+                    label = "Góra"
+                }
+                interactionButton(ButtonStyle.Primary, "down") {
+                    label = "Dół"
+                }
+                interactionButton(ButtonStyle.Primary, "right") {
+                    label = "Prawo"
+                }
+            }
+        }
+        interaction.respondPublic {  }
+
     }
 
     kord.login {
-        // we need to specify this to receive the content of messages
         @OptIn(PrivilegedIntent::class)
         intents += Intent.MessageContent
     }
